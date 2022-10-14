daa2c6e742448cddeb2525a5a3b4a2be5a4d75c3
numan <numan.demir@gmx.at>
2022-10-14 18:46:06 +0200

    Rhombus incomplete

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -77,8 +77,43 @@ public class App {
     }
 
     //todo Task 4
-    public void printRhombus(){
+    public void printRhombus() {
         // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("h: ");
+        int h = scanner.nextInt();
+        System.out.print("c: ");
+        char c = scanner.next().charAt(0);
+
+        if (h % 2 != 0) {
+            for (int i = 1; i <= (h / 2) ; i++) {       //Obere Hälfte
+                for (int j = i; j <= h / 2; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+            for (int i = 1; i <= h / 2; i++) {       //Untere Häflte
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = i; j <= h / 2; j++) {
+                    System.out.print("*");
+                }
+                for (int j = i; j <= h / 2; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }else {
+            System.out.println("Invalid number!");
+        }
     }
 
     //todo Task 5
@@ -129,7 +164,7 @@ public class App {
         System.out.print("n: ");
         int number = scanner.nextInt();
         int sum = 0;
-        while (number != 0 && number != 4) {
+        while (number != 1 && number != 4) {
             while (number > 0) {
                 sum = sum + (number % 10) * (number %10);
                 number = number / 10;
@@ -149,14 +184,14 @@ public class App {
     public static void main(String[] args){
         App exercise2 = new App();
 
-        System.out.println("Task 1: Largest Number");
+        /*System.out.println("Task 1: Largest Number");
         exercise2.largestNumber();
 
         System.out.println("\nTask 2: Stairs");
         exercise2.stairs();
 
         System.out.println("\nTask 3: Pyramide");
-        exercise2.printPyramid();
+        exercise2.printPyramid(); */
 
         System.out.println("\nTask 4: Raute");
         exercise2.printRhombus();
